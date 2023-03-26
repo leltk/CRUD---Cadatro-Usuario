114f80eb9bc206d662935df9b8b86bfdc9b7401d
unknown <leo_costab@outlook.com>
2023-03-26 20:51:01 -0300

    feat: add Entities

--- back/src/app.ts
+++ back/src/app.ts
@@ -2,13 +2,11 @@ import "express-async-errors";
 import "reflect-metadata";
 import express from "express";
 // import { UserRouter } from "./router/users.routes";
-// import { addressRouter } from "./router/address.router";
 
 const app = express();
 
 app.use(express.json());
 // app.use("/users", UserRouter);
-// app.use("/users", addressRouter);
 
 
 // app.use(errorHandler);
