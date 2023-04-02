72abd4b3b2697a36b49140467218189d44a92c6e
leltk <106624863+leltk@users.noreply.github.com>
2023-04-01 22:52:26 -0300

    Revert "Feat/contacts"

--- back/src/interfaces/session.interfaces.ts
+++
@@ -1,8 +0,0 @@
-export interface ISessionRequest {
-    email: string;
-    password: string;
-  }
-  
-  export interface ISessionResponse {
-    token: string;
-  }
--- back/src/utils/repositories.ultil.ts
+++
@@ -1,8 +0,0 @@
-import AppDataSource from "../data-source";
-import { User } from "../entities/user.entity";
-
-const usersRepository = AppDataSource.getRepository(User);
-
-export {
-  usersRepository,
-};
